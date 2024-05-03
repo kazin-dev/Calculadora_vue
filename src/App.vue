67094625b843e8a8bac7f81c6f92827a5bd46fcb
kauã gomes <kgs26121967@gmail.com>
2024-05-03 16:53:27 -0300

    script da calculadora

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
+
+
 <script setup>
+import { ref, watch } from 'vue';
+
+const numero1 = ref(0);
+const numero2 = ref(0);
+const resultado = ref(0);
+const operacao = ref('soma');
+
+const calcular = () => {
+  switch (operacao.value) {
+    case 'soma':
+      resultado.value = numero1.value + numero2.value;
+      break;
+    case 'subtracao':
+      resultado.value = numero1.value - numero2.value;
+      break;
+    case 'divisao':
+    resultado.value = numero1.value / numero2.value;
+      break;
+    case 'multiplicacao':
+    resultado.value = numero1.value * numero2.value;
+      break;
+    default:
+      console.error('Operação não suportada');
+  }
+}
+
+// Observa as mudanças nas variáveis e chama a função calcular
+watch([numero1, numero2, operacao], () => {
+  calcular();
+});
+
+
 
 </script>
 
 <template>
   <div>
-    <!-- Campo de entrada para o primeiro número -->
-    <input type="number" v-model="numero1" @input="calcularResultado">
+    <input type="number" v-model="numero1">
     
     <!-- Campo de seleção para escolher a operação aritmética -->
-    <select v-model="operacao" @change="calcularResultado">
+    <select v-model="operacao" @change="calcular">
       <option value="soma">+</option>
       <option value="subtracao">-</option>
-      <option value="multiplicacao">*</option>
-      <option value="divisao">/</option>
+      <option value="divisao"> ÷</option>
+      <option value="multiplicacao">x</option>
     </select>
     
     <!-- Campo de entrada para o segundo número -->
-    <input type="number" v-model="numero2" @input="calcularResultado">
+    <input type="number" v-model="numero2">
     
     <!-- Resultado do cálculo -->
     <p>Resultado: {{ resultado }}</p>
   </div>
 </template>
+
+
 
 <style scoped></style>
