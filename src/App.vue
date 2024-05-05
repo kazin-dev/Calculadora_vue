06616be4471683136fd55c025e1785f70fa39aa4
kauã gomes <kgs26121967@gmail.com>
2024-05-05 19:33:23 -0300

    Calculadora finalizada

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { ref, watch } from 'vue';
-import Calculadora from './components/Calculadora.vue';
 
 const numero1 = ref(0);
 const numero2 = ref(0);
@@ -8,22 +7,22 @@ const resultado = ref(0);
 const operacao = ref('soma');
 
 const calcular = () => {
-  switch (operacao.value) {
-    case 'soma':
-      resultado.value = numero1.value + numero2.value;
-      break;
-    case 'subtracao':
-      resultado.value = numero1.value - numero2.value;
-      break;
-    case 'divisao':
-    resultado.value = numero1.value / numero2.value;
-      break;
-    case 'multiplicacao':
-    resultado.value = numero1.value * numero2.value;
-      break;
-    default:
-      console.error('Ocorreu um erro');
-  }
+    switch (operacao.value) {
+        case 'soma':
+            resultado.value = numero1.value + numero2.value;
+            break;
+        case 'subtracao':
+            resultado.value = numero1.value - numero2.value;
+            break;
+        case 'divisao':
+            resultado.value = numero1.value / numero2.value;
+            break;
+        case 'multiplicacao':
+            resultado.value = numero1.value * numero2.value;
+            break;
+        default:
+            console.error('Ocorreu um erro');
+    }
 }
 
 watch([numero1, numero2, operacao], () => {
@@ -34,14 +33,20 @@ watch([numero1, numero2, operacao], () => {
 <template>
   <h1>Calculadora Aritmética</h1>
   <div class="container">
-    <Calculadora :resultad/>
+    <input type="number" v-model="numero1">
+    <select v-model="operacao">
+        <option value="soma">+</option>
+        <option value="subtracao">-</option>
+        <option value="divisao"> ÷</option>
+        <option value="multiplicacao">x</option>
+    </select>
+    <input type="number" v-model="numero2">
+    <p>Resultado: {{ resultado }}</p>
   </div>
 </template>
 
-
-
 <style scoped>
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
@@ -50,7 +55,7 @@ watch([numero1, numero2, operacao], () => {
 
 
 
-.container{
+.container {
   display: flex;
   justify-content: center;
   align-items: center;
@@ -58,14 +63,14 @@ watch([numero1, numero2, operacao], () => {
   background-color: rgb(235, 209, 65);
 }
 
-input, select{
+input,
+select {
   padding: 12px;
   border-radius: 10px;
   margin: 0 18px;
 }
 
-p{
+p {
   font-size: 24px;
 }
-
 </style>
--- vite.config.js
+++ vite.config.js
@@ -4,22 +4,19 @@ import Vue from '@vitejs/plugin-vue'
 export default defineConfig({
     base: '/',
 
-    // Configurações do servidor de desenvolvimento
     server: {
-        host: 'localhost', // Host do servidor
-        port: 3000, // Porta do servidor
-        open: true // Abrir automaticamente o navegador
+        host: 'localhost',
+        port: 3000,
+        open: true 
     },
 
-    // Configurações de construção para produção
     build: {
-        outDir: 'dist', // Diretório de saída para os arquivos construídos
-        assetsDir: 'assets', // Diretório dos assets
-        manifest: true, // Geração do arquivo de manifesto
-        minify: 'terser' // Opções de minificação do código
+        outDir: 'dist', 
+        assetsDir: 'assets',
+        manifest: true, 
+        minify: 'terser'
     },
 
-    // Variáveis globais definidas no ambiente de construção
     define: {
         __VUE_OPTIONS_API__: true,
         __VUE_PROD_DEVTOOLS__: false
